ececca2529e3d42e25165c09109d2ee2c689cef1
vishenosik <nik.kuzya192@mail.ru>
2025-11-07 15:43:48 +0400

    feat(file service): Generate grpc public libs. Add taskfile & scripts.

---
+++ scripts/generate.go.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+GOLANG_GEN_DIR=$1
+PROTOS_DIR=$2
+
+for VersionDir in "$PROTOS_DIR"/*; do
+    API_VERSION=$(basename "${VersionDir}")
+
+    for file in "$VersionDir"/*.proto; do
+
+        SERVICE=$(basename "${file%.*}" .proto)
+
+        dir="$GOLANG_GEN_DIR/$API_VERSION/$SERVICE"
+        if ! [[ -d $dir ]]; then
+            mkdir -p $dir
+        fi
+
+        protoc -I $PROTOS_DIR/$API_VERSION "$PROTOS_DIR/$API_VERSION/$SERVICE.proto" --go_out=$dir --go_opt=paths=source_relative --go-grpc_out=$dir --go-grpc_opt=paths=source_relative
+        echo "Generating SERVICE: $SERVICE version: $API_VERSION Success"
+
+    done
+done
